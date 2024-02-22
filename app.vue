7988f040b5e1d60d01e81dbc2280ad26ad44acb6
vnphu <vonhuphu117@gmail.com>
2024-02-22 10:07:38 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="card flex flex-wrap gap-3 p-fluid">
+    <div class="flex-auto">
+      <label for="integeronly" class="font-bold block mb-2"> Integer Only </label>
+      <InputNumber v-model="value1" inputId="integeronly" />
+    </div>
+    <div class="flex-auto">
+      <label for="withoutgrouping" class="font-bold block mb-2"> Without Grouping </label>
+      <InputNumber v-model="value2" inputId="withoutgrouping" :useGrouping="false" />
+    </div>
+    <div class="flex-auto">
+      <label for="minmaxfraction" class="font-bold block mb-2"> Min-Max Fraction Digits </label>
+      <InputNumber v-model="value3" inputId="minmaxfraction" :minFractionDigits="2" :maxFractionDigits="5" />
+    </div>
+    <div class="flex-auto">
+      <label for="minmax" class="font-bold block mb-2"> Min-Max Boundaries </label>
+      <InputNumber v-model="value4" inputId="minmax" :min="0" :max="100" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const value1 = ref(42723);
+const value2 = ref(58151);
+const value3 = ref(2351.35);
+const value4 = ref(50);
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "path";
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+  css: ["~/assets/styles/base.css"],
+  primevue: {
+    unstyled: true,
+    importPT: { from: path.resolve(__dirname, "./presets/lara/") }, //import and apply preset
+  },
+});
